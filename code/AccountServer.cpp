db63843e94a3cb9da9f058312d40bf75efff0f04
Benjamin <Benjamin.Engelman@ulb.ac.be>
2017-02-11 12:11:32 +0100

    added AccountServer.cpp

---
+++ code/AccountServer.cpp
@@ -0,0 +1,13 @@
+//
+// Created by benjamin on 11/02/17.
+//
+#include "AccountServer.hpp"
+
+
+AccountServer::AccountServer(int port) {}
+
+void AccountServer::run() {}
+
+void AccountServer::attemptCreateAccount(std::string name, std::string password) {}
+
+void AccountServer::checkCredentials(std::string name, std::string password) {}
