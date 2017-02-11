4cf12559830458a135b47cd2568d34cdeb1a5667
Benjamin <Benjamin.Engelman@ulb.ac.be>
2017-02-11 13:44:39 +0100

    update

--- code/Server.cpp
+++ code/Server.cpp
@@ -45,6 +45,10 @@ int Server::accept_connection() {
     return accept(socket_fd, (struct sockaddr *) &client_addr, &sin_size);
 }
 
+void Server::add_new_client(int his_socket) {
+    client_sockets.push_back(his_socket);
+}
+
 struct sockaddr_in Server::build_address() {
     struct sockaddr_in my_address;
     my_address.sin_family = AF_INET;
