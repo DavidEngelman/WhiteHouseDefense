1df764b2d739ab15e852f1c06de3cbc1fed28f01
Benjamin <Benjamin.Engelman@ulb.ac.be>
2017-02-11 12:05:14 +0100

    added AccountServer.cpp

--- code/AccountServer.hpp
+++ code/AccountServer.hpp
@@ -2,6 +2,9 @@ class AccountServer : Server {
 
 
 public:
+
+    void run() override ;
+
 	void attemptCreateAccount(string name, string password);
 
 	void checkCredentials(string name, string password);
