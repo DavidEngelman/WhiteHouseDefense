ba1a07c07ef15053808c09a6d048dce0bd569fe3
harokb <harokb@gmail.com>
2017-02-12 12:12:22 +0100

    Rajouter un struct Credentials pour rendre le code plus lisible + reformat.

--- code/AccountServer.cpp
+++ code/AccountServer.cpp
@@ -5,31 +5,27 @@
 #include "AccountServer.hpp"
 
 
-AccountServer::AccountServer(int port, const char* databaseName): Server(port), myDatabase(Database(databaseName)){}
+AccountServer::AccountServer(int port, const char *databaseName) : Server(port), myDatabase(Database(databaseName)) {}
 
-void AccountServer::run(){
+void AccountServer::run() {
 
     int newClient;
 
-    while (1){
+    while (1) {
         newClient = accept_connection();
         //add_new_client(newClient); Je laisse ca la au cas ou
 
-        if ( !fork() ){
-
-            char *username_password[500];
-            std::vector<std::string> username_password_vect;
+        if (!fork()) {
+            char username_password[BUFFER_SIZE];
+            Credentials credentials;
 
             //Get the the username and password from client
             receive_message(newClient, (char *) username_password);
 
             //Process the username and password
-            username_password_vect = get_username_and_password((char *) username_password);
+            get_username_and_password((char *) username_password, &credentials);
 
-            //Account creation
-            attemptCreateAccount(username_password_vect[0], username_password_vect[1]);
-
-
+            attemptCreateAccount(credentials.username, credentials.password);
         }
 
         //TODO: Finir ça
@@ -39,66 +35,50 @@ void AccountServer::run(){
     }
 }
 
-std::vector<std::string> AccountServer::get_username_and_password (char* data){
-
+void AccountServer::get_username_and_password(char *data, Credentials *credentials) {
     /*
      *
      * Le format de data doit être "username,password;"
 
-     * Return un tableau de deux composante:
-     * la première : le username
-     * la deuxième : le password
-
-     * */
-
-    std::vector<std::string> username_password(2);
-
-
+     * Remplis le struct Credentials avec les données dans data
+     */
     int i = 0;
     char currentChar = data[0];
 
-    while (currentChar != ','){
-
-        //extracting username
-        username_password[0] += currentChar;
-        currentChar = data[i+1];
+    //extracting username
+    while (currentChar != ',') {
+        credentials->username += currentChar;
+        currentChar = data[i + 1];
 
         i++;
-
     }
 
-    currentChar = data[i+1]; // passe la virgule
+    currentChar = data[i + 1]; // passe la virgule
     i++;
 
-    while (currentChar != ';'){
-
-        //extracting password
-        username_password[1] += currentChar;
-        currentChar = data[i+1];
+    //extracting password
+    while (currentChar != ';') {
+        credentials->password += currentChar;
+        currentChar = data[i + 1];
 
         i++;
-
     }
-
-    return username_password;
 }
 
 //Partie Register
 
-void AccountServer::insert_account_in_db(std::string username, std::string password){
+void AccountServer::insert_account_in_db(std::string username, std::string password) {
 
-    if (myDatabase.insert_account(username, password) == -1 ){
+    if (myDatabase.insert_account(username, password) == -1) {
         perror("Account creation");
         //TODO: Faudra dire au client que y a un problème (ex: le username est deja utilisé)
-    }
-    else{
+    } else {
         //TODO: Faudra dire au client que ca a marché
     }
 }
 
 void AccountServer::attemptCreateAccount(std::string username, std::string password) {
-
-    insert_account_in_db (username, password);
+    insert_account_in_db(username, password);
 }
 
 //Partie Login
--- code/AccountServer.hpp
+++ code/AccountServer.hpp
@@ -2,22 +2,30 @@
 #include <string.h>
 #include "Database.hpp"
 
+static const int BUFFER_SIZE = 500;
+
+typedef struct Credentials {
+    std::string username;
+    std::string password;
+} Credentials;
+
 class AccountServer : public Server {
 
 private:
 
-	Database myDatabase;
+    Database myDatabase;
 
 public:
 
-    AccountServer(int port, const char* databaseName);
+    AccountServer(int port, const char *databaseName);
 
     void run() override;
 
-	std::vector<std::string> get_username_and_password (char* data);
+    void get_username_and_password(char *data, Credentials *credentials);
 
-	void insert_account_in_db(std::string username, std::string password);
-	void attemptCreateAccount(std::string username, std::string password);
+    void insert_account_in_db(std::string username, std::string password);
 
-	void checkCredentials(std::string username, std::string password);
+    void attemptCreateAccount(std::string username, std::string password);
+
+    void checkCredentials(std::string username, std::string password);
 };
