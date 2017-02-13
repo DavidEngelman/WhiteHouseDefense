2773538fb97880afe99f6e7900270e6008098442
Benjamin <Benjamin.Engelman@ulb.ac.be>
2017-02-13 19:51:08 +0100

    added methods account

--- code/Account.cpp
+++ code/Account.cpp
@@ -1,3 +1,17 @@
 #include "Account.hpp"
 
+//getters
+
+std::string Account::getUsername() {return creds.getUsername();}
+std::string Account::getPassword() {return creds.getPassword();}
+FriendList Account::getFriendList() {return friendList;}
+int Account::getNbOfVictories() {return nbOfVictories;}
+int Account::getNbPnjKilled() { return nbPnjKilled;}
+
+//setters
+
+void Account::setPassword(std::string password) {creds.setPassword(password);}
+void Account::setUsername(std::string username) {creds.setUsername(username);}
+void Account::setNbOfVictories(int nbr) {nbOfVictories = nbr;}
+void Account::setNbPnjKilled(int nbr) {nbPnjKilled = nbr;}
 
