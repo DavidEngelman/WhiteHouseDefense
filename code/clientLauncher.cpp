1634c14232037e203537297e7d074e924e9cbea0
David <David.Engelman@ulb.ac.be>
2017-02-13 15:46:27 +0100

    create ClientLauncher.cpp

--- code/clientLauncher.cpp
+++ code/clientLauncher.cpp
@@ -1,4 +1,9 @@
-//
-// Created by david on 13/02/17.
-//
+#include "Manager.h"
 
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    Manager myTestManager = Manager(5555, "127.0.0.1");
+
+    return 0;
+}
