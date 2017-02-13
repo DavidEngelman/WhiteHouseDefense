e39483f1bbb36c04d5fcb39bd9c471b86f892f69
Benjamin <Benjamin.Engelman@ulb.ac.be>
2017-02-13 22:54:03 +0100

    fixed clientLauncher

--- code/clientLauncher.cpp
+++ code/clientLauncher.cpp
@@ -10,18 +10,21 @@ int main(int argc, char *argv[]) {
     }
 
     int choice = 0;
+    bool ok = false;
 
-    while (choice != 1 || choice != 2) {
+    while (!ok) {
         std::cout << "Press 1 to log in or 2 to register a new account" << std::endl;
         std::cin >> choice;
 
-
-        if (choice == 1)
+        if (choice == 1) {
             LoginManager myLoginManager = LoginManager(5555, argv[1]);
+            ok = true;
+        }
 
-        else if (choice == 2)
+        else if (choice == 2) {
             RegisterManager myRegisterManager = RegisterManager(5555, argv[1]);
-
+            ok = true;
+        }
         else
             std::cout << "Please enter 1 or 2" << std::endl;
 
