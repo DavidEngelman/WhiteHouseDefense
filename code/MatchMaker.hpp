e0b9c22c5a8793ef8a5c4b76f6a09c36eb071a6b
Benjamin <Benjamin.Engelman@ulb.ac.be>
2017-02-15 18:04:33 +0100

    cleaned comments

--- code/MatchMaker.hpp
+++ code/MatchMaker.hpp
@@ -25,18 +25,6 @@ private:
     PendingMatch timedPendingMatch;
     PendingMatch teamPendingMatch;
 
-    // Je sais pas si c'est peut etre pas mieux 3 listes, une pour chaque
-    // mode de jeu
-
-    // Pourquoi est-ce qu'il faut 3 listes?
-    // Ca ne suffirait pas d'avoir un
-    // mode1PendingMatch
-    // mode2PendingMatch
-    // mode3PendingMatch
-    // (J'ai oublié le nom des modes, mais ça devrait etre qq chose ddu genre)
-
-    //hein oui je sais pas pq j'ai dit ça :D
-
     void addPlayerToPendingMatch(PlayerConnection player_connection, std::string mode);
 
     PendingMatch &getMatch(std::string mode);
