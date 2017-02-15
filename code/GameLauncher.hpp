17cf910d5729509548982c113524338d9472dd66
harokb <harokb@gmail.com>
2017-02-15 20:49:45 +0100

    Started GameLauncher.

--- code/GameLauncher.hpp
+++ code/GameLauncher.hpp
@@ -2,12 +2,17 @@
 #define GAMELAUNCHER_HPP
 
 #include <string>
+#include "Networking.h"
+#include "Manager.hpp"
+#include "MatchMaker.hpp" // Vraiment moche, j'utilise juste parce que j'ai besoin du GAME_START_STRING
+// TODO: faudra probablement faire un fichier qui reprend tous les strings
 
-class GameLauncher {
+class GameLauncher : Manager {
 
 
 public:
-	void sendJoinRequest(std::string account);
+    GameLauncher(int port, char* address);
+    void sendJoinRequest(std::string mode);
 };
 
 #endif
