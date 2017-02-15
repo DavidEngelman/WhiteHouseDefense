cd49d5adfbea784286e2493ccc1a30194a83f618
David <David.Engelman@ulb.ac.be>
2017-02-15 11:22:35 +0100

    update Account

--- code/Account.cpp
+++ code/Account.cpp
@@ -2,14 +2,15 @@
 
 //getters
 
-std::string Account::getUsername() {return creds.getUsername();}
-std::string Account::getPassword() {return creds.getPassword();}
-FriendList Account::getFriendList() {return friendList;}
-int Account::getNbOfVictories() {return nbOfVictories;}
-int Account::getNbPnjKilled() { return nbPnjKilled;}
+const Credentials &Account::getCreds() const {return creds;}
+FriendList Account::getFriendList() const {return friendList;}
+int Account::getNbOfVictories() const {return nbOfVictories;}
+int Account::getNbPnjKilled() const { return nbPnjKilled;}
+int Account::getId() const {return id;}
 
 //setters
 
+void Account::setId(int id) {Account::id = id;}
 void Account::setPassword(std::string password) {creds.setPassword(password);}
 void Account::setUsername(std::string username) {creds.setUsername(username);}
 void Account::setNbOfVictories(int nbr) {nbOfVictories = nbr;}
