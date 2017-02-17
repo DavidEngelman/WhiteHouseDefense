8ccbfddfa2fb15df66b10d1103994250fbce9dab
harokb <harokb@gmail.com>
2017-02-17 15:52:31 +0100

    Matchmaker now compiles.

--- code/common/Strings.hpp
+++ code/common/Strings.hpp
@@ -2,5 +2,9 @@
 #define PROJET_STRINGS_HPP
 
 #define GAME_STARTING_STRING "GameStarting"
+#define CLASSIC_MODE "ClassicMode"
+#define TIMED_MODE "TimedMode"
+#define TEAM_MODE "TeamMode"
+
 
 #endif //PROJET_STRINGS_HPP
--- code/server/MatchMaker.hpp
+++ code/server/MatchMaker.hpp
@@ -2,20 +2,32 @@
 #define MATCHMAKER_HPP
 
 #include "Server.hpp"
+#include "PendingMatch.h"
+#include "MatchmakingCommand.hpp"
+#include "../common/Strings.hpp"
 #include <string>
 
 class MatchMaker : public Server {
 
 private:
-	int pendingMatches;
+
+    PendingMatch classicPendingMatch;
+    PendingMatch timedPendingMatch;
+    PendingMatch teamPendingMatch;
+
+    void announceMatchStart(PlayerConnection playerConnection);
+    void launchGameServer(PendingMatch match);
+    void launchMatch(PendingMatch match);
+    PendingMatch& getMatch(std::string mode);
+    void addPlayerToPendingMatch(PlayerConnection player_connection, std::string mode);
+    void get_and_process_command(int socket_fd);
+
 
 public:
 
-	void getNewClients();
+    MatchMaker(int port);
 
-	void getPendingMatches(std::string mode);
-
-	void addPendingMatch(std::string mode);
+    void run();
 };
 
 #endif
