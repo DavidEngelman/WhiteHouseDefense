253188e7e17c03740c07c79bfaa2ff0a4a677140
harokb <harokb@gmail.com>
2017-02-18 19:41:28 +0100

    Moved PlayerConnection into its own class.

---
+++ code/server/PlayerConnection.hpp
@@ -0,0 +1,8 @@
+#ifndef PROJET_PLAYERCONNECTION_HPP
+#define PROJET_PLAYERCONNECTION_HPP
+
+struct PlayerConnection {
+    int socket_fd;
+    int player_id;
+};
+#endif //PROJET_PLAYERCONNECTION_HPP
