ff96055902145956ee6c4d193a6ccdfeb95df963
Benjamin <Benjamin.Engelman@ulb.ac.be>
2017-02-18 21:06:32 +0100

    todotodo

--- code/server/GameServer.cpp
+++ code/server/GameServer.cpp
@@ -13,3 +13,7 @@ void GameServer::sendGameStateToPlayer(PlayerConnection &connection) {
     const std::string &serialized_game_state = gameState.serialize();
     send_message(connection.socket_fd, serialized_game_state.c_str());
 }
+
+void GameServer::getReceivedChanges() {
+    //TODO
+}
