df3e3f5566dccad4da4dbfa51e51ec5143bd7ca7
harokb <harokb@gmail.com>
2017-02-18 18:43:21 +0100

    Added GameServer to CMake and added comment.

--- code/server/GameServer.hpp
+++ code/server/GameServer.hpp
@@ -1,16 +1,29 @@
 
 #include "Server.hpp"
+#include "../common/GameState.hpp"
 #include <string.h>
 
 
 class GameServer : public Server {
 
 public:
-	GameState serverGamesSate;
+    GameState serverGamesSate;
 
-	void getReceivedChanges();
+    void getReceivedChanges();
 
-	void parseCommands(changes); //je pense que c'est inutile avec notre class command
+    void parseCommands(changes); //je pense que c'est inutile avec notre class command
 
-	void sendGameState(gameState);
+    //oui, je crois que une des deux fonction (soit getReceivedChanges, soit parseCommands) ne servira a rien
+    // dans ma tete, la partie du code de la boucle principale qui s'occupe des commandes du client sera
+    // qq chose de ce genre
+
+    /*
+     * while (1){
+     *      TowerCommand (ou un truc du genre) command = getCommand();
+     *      modifyMap(command);
+     * }
+     */
+
+
+    void sendGameState(gameState);
 };
