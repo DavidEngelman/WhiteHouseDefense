c6797673e04ce1d9cef43b29831a7aed4a039201
harokb <harokb@gmail.com>
2017-02-18 20:23:36 +0100

    Modifé les signatures, et ajouté quelques commentaires.

--- code/common/GameState.hpp
+++ code/common/GameState.hpp
@@ -9,25 +9,32 @@
 
 class GameState {
 
-	std::vector<PlayerState> player_states;
-	std::vector<Tower>       towers;
-	std::vector<PNJ>         pnjs;
+    std::vector<PlayerState> player_states;
+    std::vector<Tower> towers;
+    std::vector<PNJ> pnjs;
 
-	Map map;
-
-
-
+    Map map;
 public:
 
-	void decrease_player_hp(PlayerState& player, int amount);
-    void increase_player_hp(PlayerState& player, int amount);
+    void decrease_player_hp(PlayerState &player, int amount);
 
-    void add_tower();
+    void increase_player_hp(PlayerState &player, int amount);
 
+    void add_tower(Position position);
+
+    // Oui, cette fonctio ne sert plus à grand chose. Je crois qu'on peut l'eliminer
     void applyChanges(std::string changes); //Pourquoi on a et ApplyChanges et UpdateGameState ?
 
-	void updateGameState(int commands);
+    // Cette fonction aussi
+    void updateGameState(int commands);
+
+    // Par contre il faudra une fonction pour faire évoluer l'état du jeu lors d'une vague.
+    // genre: void updateGameState(int timeEllaped);
+    // ou tout simplement: void updateGameState();
+    // où est cette classe comptabilise le temps écoulé et fait les calculs pour voir de combien
+    // les mexicains ont avancé (entre autres)
 
     std::string serialize();
 };
+
 #endif
