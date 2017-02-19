6043e72ff4212da55e76c823a8676517a2c7c180
Jepsiko <Jepsiko@Gmail.com>
2017-02-19 13:55:27 +0100

    modifications de la classe abstract tower et la classe position est useless

--- code/common/AbstractTower.hpp
+++ code/common/AbstractTower.hpp
@@ -2,12 +2,14 @@
 #define PROJET_ABSTRACTTOWER_H
 
 #include "../common/Position.hpp"
+#include "PNJ.hpp"
 
 class AbstractTower {
 
 private:
 
 	int owner; //ID du joueur qui possède la tour
+	int typeOfTower;
 	int price;
 	int damage;
 	Position position;
@@ -16,13 +18,17 @@ public:
 
 	virtual void shoot(PNJ target);
 
+	/* The price of the tower must be a constant in the constructor
 	int getPrice() const;
 
 	void setPrice(int price);
+	*/
 
+	/* The position is defined in the map class
 	Position getPosition() const;
 
 	void setPosition(int x, int y);
+	*/
 };
 
 #endif // PROJET_ABSTRACTTOWER_H
