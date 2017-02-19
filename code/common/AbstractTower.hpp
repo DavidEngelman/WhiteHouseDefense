bf1f8d149bc0b1aada4b5611f9b4a392faaec041
egavrilo <egavriloaia@gmail.com>
2017-02-19 14:39:11 +0100

    made price in AbstractTower constant

--- code/common/AbstractTower.hpp
+++ code/common/AbstractTower.hpp
@@ -9,25 +9,24 @@ class AbstractTower {
 private:
 
     int owner; // player ID
-    int price;
+    const int price;
     Position position;
 
 public:
 
     virtual void shoot(PNJ target); // HAS TO BE DEFINED BY INHERITORS
 
-    // The price of a tower must be a constant
     virtual int getOwner() const;
 
     virtual void setOwner(int owner);
 
-    //The position is defined in the map class
     virtual int getPrice() const;
 
     virtual void setPrice(int price);
 
     virtual Position getPosition() const;
 
+    //The position is defined in the map class
     virtual void setPosition(Position position);
 };
 
