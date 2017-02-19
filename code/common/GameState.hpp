5fed59bcb38d52bc3067931de3bc198c9bc8624b
harokb <harokb@gmail.com>
2017-02-19 18:18:28 +0100

    Ajouté une fonction isWaveFinished au GameState.

--- code/common/GameState.hpp
+++ code/common/GameState.hpp
@@ -43,6 +43,7 @@ public:
     // où est cette classe comptabilise le temps écoulé et fait les calculs pour voir de combien
     // les mexicains ont avancé (entre autres)
 
+    bool isWaveFinished();
 };
 
 #endif
