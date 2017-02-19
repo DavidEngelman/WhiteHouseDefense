fdcc02f88041cf71dce8e8dfbf8d3c3a55dd0efb
egavrilo <egavriloaia@gmail.com>
2017-02-19 18:22:14 +0100

    AttackTower.cpp small correction

--- code/common/AttackTower.cpp
+++ code/common/AttackTower.cpp
@@ -4,4 +4,4 @@ int AttackTower::getDamageAmount() const { return damageAmount; }
 
 void AttackTower::setDamageAmount(int damageAmount) { this->damageAmount = damageAmount; }
 
-virtual void AttackTower::dealDamageTo(PNJ pnj) { pnj.receiveDamage(this->getDamageAmount()); }
+void AttackTower::dealDamageTo(PNJ pnj) { pnj.receiveDamage(this->getDamageAmount()); }
