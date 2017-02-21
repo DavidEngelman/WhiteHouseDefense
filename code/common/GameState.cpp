fe5763eee3c5fddbfb8b992b8aec40fd8f9f3f49
harokb <harokb@gmail.com>
2017-02-21 19:21:07 +0100

    corrigé les for loop.

--- code/common/GameState.cpp
+++ code/common/GameState.cpp
@@ -11,16 +11,16 @@ void GameState::increase_player_hp(PlayerState& player, int amount){
 std::string GameState::serialize() {
     std::string serialized_me;
 
-    for (PlayerState pstate: player_states){
+    for (PlayerState& pstate: player_states){
         // C'est un peu bizarre comme fonction. Intuitivement, on dirait qu'il va
         // mettre le resultat dans serialized_me, au lieu de faire un append.
         // C'est ce que j'ai pensé quand j'ai vu ça
         serialized_me += pstate.serialize();
     }
-    for (AbstractTower tower: towers){
+    for (AbstractTower& tower: towers){
         serialized_me += tower.serialize();
     }
-    for (Wave wave: waves){
+    for (Wave& wave: waves){
         serialized_me += wave.serialize();
     }
 
@@ -28,8 +28,10 @@ std::string GameState::serialize() {
 }
 
 void GameState::add_tower(Position position) {
-    void GameState::add_tower() {
-        towers.push_back(Tower tower);
+    // TODO: il faut modifier cette fonction, car on ne sait pas de quel type de tour on parle
+    // il faut soit prendre un parametre de plus (le type), soit modifier la signature pour
+    // qu'elle prenne une AbstractTower
+//        towers.push_back(Tower tower);
 }
 
 bool GameState::is_only_one_alive() {
