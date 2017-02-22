97b693cd02a86a4907040ab5f50becc082675016
harokb <harokb@gmail.com>
2017-02-22 15:01:03 +0100

    Trouvé origine du bug.

--- code/client/App.cpp
+++ code/client/App.cpp
@@ -7,7 +7,7 @@ App::App(char* server_ip_addr) : current_manager(new WelcomeManager(server_ip_ad
 
 void App::transition(AbstractManager *new_manager) {
     std::cout << "transition" << std::endl;
-    delete(current_manager);
+    delete(current_manager);  // Le bug est declenché par cette ligne
     std::cout << "delete ok" << std::endl;
 
     current_manager = new_manager;
