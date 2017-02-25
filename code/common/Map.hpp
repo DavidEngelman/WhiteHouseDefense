325fde3a34677243d5de40de9a6c25f24fc7942b
harokb <harokb@gmail.com>
2017-02-25 13:36:05 +0100

    Added seed field to Map (need to send it to client).

--- code/common/Map.hpp
+++ code/common/Map.hpp
@@ -22,6 +22,11 @@
 class Map {
 private:
     int matrix[SIZE][SIZE];
+    const unsigned int _seed;
+public:
+    unsigned int get_seed() const;
+
+private:
 
     void generateRandomMatrix();
     void initMap();
