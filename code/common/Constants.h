0a11406d8b1131bd67165fba57836f8ea0445418
harokb <harokb@gmail.com>
2017-02-25 12:44:37 +0100

    Solved compilation error.

--- code/common/Constants.h
+++ code/common/Constants.h
@@ -5,7 +5,7 @@ static const int NORTH = 0;
 static const int EAST = 1;
 static const int SOUTH = 2;
 static const int WEST = 3;
-static const int DIRECTIONS[] = [NORTH, EAST, SOUTH, WEST];
+static const int DIRECTIONS[] = {NORTH, EAST, SOUTH, WEST};
 
 static const int PNJ_STARTING_HEALTHPOINTS= 100;
 static const int PNJ_STARTING_MOVESPEED = 100;
