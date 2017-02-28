41d2d0a1b954b48f0606f0ddefeb9c9491cdb45b
Benjamin <Benjamin.Engelman@ulb.ac.be>
2017-02-28 16:48:45 +0100

    fixed buy/sellRequests

--- code/client/GameManager.cpp
+++ code/client/GameManager.cpp
@@ -85,13 +85,13 @@ bool GameManager::checkValidity(Position towerPos, GameState& gamestate) {
 
 
 void GameManager::sendBuyRequest(Position towerPos, std::string towerType) {
-    std::string message = PLACE_TOWER_COMMAND_STRING + "," + towerType + "," + std::to_string(towerPos.getX()) + std::to_string(towerPos.getY())+";";
+    std::string message = PLACE_TOWER_COMMAND_STRING + "," + std::to_string(quadrant) + towerType + "," + std::to_string(towerPos.getX()) + std::to_string(towerPos.getY())+";";
     send_message(server_socket, message.c_str());
 }
 
 void GameManager::sendSellRequest(Position towerPos) {
     std::string type = "NULL";
-    std::string message = DELETE_TOWER_COMMAND_STRING + "," + type + std::to_string(towerPos.getX()) + std::to_string(towerPos.getY())+";";
+    std::string message = DELETE_TOWER_COMMAND_STRING + "," + std::to_string(quadrant) + type + std::to_string(towerPos.getX()) + std::to_string(towerPos.getY())+";";
     send_message(server_socket, message.c_str());
 }
 
