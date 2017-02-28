efc9ed1491ccc6fd1355d4f905d01cd92adc794f
David <David.Engelman@ulb.ac.be>
2017-02-28 16:39:07 +0100

    j'ai rajouter les constantes liés a l'attackTower dans constant.h

--- code/common/Constants.h
+++ code/common/Constants.h
@@ -21,4 +21,8 @@ static const int GOLD_EARNED_BY_SECOND= 1;
 // Ça doit être 100.0, sinon c'est une division entiere et le resultat est 0
 static const float PERCENTAGE_RECOVERED_MONEY= (const float) (50 / 100.0);
 
+const int ATTACK_TOWER_PRICE = 10;
+const int ATTACK_TOWER_RANGE = 4;
+const int ATTACK_TOWER_DAMAGE = 20;
+
 #endif //PROJET_CONSTANTS_H
