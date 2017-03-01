1b5a426a31a835f699f657a5bb980ed5cc6501e6
harokb <harokb@gmail.com>
2017-03-01 15:24:58 +0100

    Added pause & resume methods to Timer.

--- code/server/Timer.cpp
+++ code/server/Timer.cpp
@@ -1,22 +1,44 @@
+#include <exception>
+#include <stdexcept>
 #include "Timer.h"
 
+Timer::Timer(): numOfElapsedMilisecondsInPreviousIntervals(0), isPaused(true) {}
+
 void Timer::start() {
-    gettimeofday(&start_timeval, 0);
+    gettimeofday(&last_pause_timeval, 0);
+    isPaused = false;
 }
 
 void Timer::reset() {
+    numOfElapsedMilisecondsInPreviousIntervals = 0;
+    isPaused = false;
     start();
 }
 
-int Timer::elapsedTimeInMiliseconds() {
-    struct timeval current_timeval;
-    gettimeofday(&current_timeval, 0);
+void Timer::pause() {
+    numOfElapsedMilisecondsInPreviousIntervals += getMilisecondsSinceLastCurrentIntervalStart();
+    isPaused = true;
+}
 
-    double elapsedTimeInMilliseconds = (current_timeval.tv_sec - start_timeval.tv_sec) * 1000;
-    elapsedTimeInMilliseconds += (current_timeval.tv_usec - start_timeval.tv_usec) / 1000;
-    return (int) elapsedTimeInMilliseconds;
+void Timer::resume() {
+    gettimeofday(&last_pause_timeval, 0);
+    isPaused = false;
+}
+
+int Timer::elapsedTimeInMiliseconds() {
+    if (isPaused) throw std::logic_error("Timer is paused");
+    return numOfElapsedMilisecondsInPreviousIntervals + getMilisecondsSinceLastCurrentIntervalStart();
 }
 
 int Timer::elapsedTimeInSeconds() {
     return elapsedTimeInMiliseconds() / 1000;
+}
+
+int Timer::getMilisecondsSinceLastCurrentIntervalStart() {
+    struct timeval current_timeval;
+    gettimeofday(&current_timeval, 0);
+
+    double elapsedTimeInMilliseconds = (current_timeval.tv_sec - last_pause_timeval.tv_sec) * 1000;
+    elapsedTimeInMilliseconds += (current_timeval.tv_usec - last_pause_timeval.tv_usec) / 1000;
+    return (int) elapsedTimeInMilliseconds;
 }
