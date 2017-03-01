205bd714f7310e4a5a132a3ca5dd9342ddc92b8c
NoLan314 <thibaut.colson1996@gmail.com>
2017-03-01 15:13:51 +0100

    rajout dessin spectatorUI

--- code/client/SpectatorUI.cpp
+++ code/client/SpectatorUI.cpp
@@ -3,10 +3,13 @@
 
 #include <iostream>
 #include "SpectatorUI.hpp"
+#include "Drawing.hpp"
 
 void SpectatorUI::displaySpectatorUI() {
     int i=0;
+    Drawing::drawWhiteHouse("SPECTATOR SCREEN");
     //for type de partie;
+        //drawTitleGameType(gameType);
     //std::cout<<type de partie<<std//endl;
     //for()//pour chaque partie
     //std::cout<<i<<") "<<partie<<"("<<nom_joueur<<")"<<std::endl;
@@ -19,4 +22,14 @@ int SpectatorUI::inputSpectatorUI() {
     std::cin >> choice;
 
     return choice;
+}
+
+void SpectatorUI::drawTitleGameType(std::string gameType) {
+    unsigned long nbSpace = (36 - gameType.length()) / 2;
+    std::cout << "   ======================================   " << std::endl;
+    std::cout << "   |" << std::string(nbSpace, ' ') << gameType << std::string(nbSpace, ' ');
+    if (gameType.length() % 2 == 1) std::cout << " ";
+    std::cout << "|" << std::endl;
+    std::cout << "   ======================================   " << std::endl;
+
 }
