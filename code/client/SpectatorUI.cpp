5809346cca2aefe5e54575fdfb85e5629c4fec18
NoLan314 <thibaut.colson1996@gmail.com>
2017-03-01 14:54:23 +0100

    spectatorUI

--- code/client/SpectatorUI.cpp
+++ code/client/SpectatorUI.cpp
@@ -4,9 +4,19 @@
 #include <iostream>
 #include "SpectatorUI.hpp"
 
-void SpectatorUI::display() {
+void SpectatorUI::displaySpectatorUI() {
     int i=0;
-    for()//pour chaque partie
-    std::cout<<i<<") "<<partie<<"("<<nom_joueur<<")"<<std::endl
+    //for type de partie;
+    //std::cout<<type de partie<<std//endl;
+    //for()//pour chaque partie
+    //std::cout<<i<<") "<<partie<<"("<<nom_joueur<<")"<<std::endl;
 
+}
+
+int SpectatorUI::inputSpectatorUI() {
+    int choice;
+    std::cout<<"Enter the number of the game that you would like to see: ";
+    std::cin >> choice;
+
+    return choice;
 }
