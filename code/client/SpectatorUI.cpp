d8000b49376f08bbd8525b6ddf21e8e4e85142cb
NoLan314 <thibaut.colson1996@gmail.com>
2017-03-01 14:45:20 +0100

    debut display spectator

--- code/client/SpectatorUI.cpp
+++ code/client/SpectatorUI.cpp
@@ -1,4 +1,12 @@
 //
 //
 
+#include <iostream>
 #include "SpectatorUI.hpp"
+
+void SpectatorUI::display() {
+    int i=0;
+    for()//pour chaque partie
+    std::cout<<i<<") "<<partie<<"("<<nom_joueur<<")"<<std::endl
+
+}
--- code/client/SpectatorUI.hpp
+++ code/client/SpectatorUI.hpp
@@ -6,6 +6,7 @@
 
 
 class SpectatorUI {
+    void display();
 
 };
 
