fa975ee52832c84fa2ab1590a699024342539630
David <David.Engelman@ulb.ac.be>
2017-03-02 17:46:29 +0100

    update

--- code/common/Constants.h
+++ code/common/Constants.h
@@ -11,8 +11,8 @@ static const std::string QUADRANT_NAMES[4] = {"NORTH", "EAST", "SOUTH", "WEST"};
 static const int PNJ_STARTING_HEALTHPOINTS= 100;
 static const int PNJ_STARTING_MOVESPEED = 100;
 
-static const int  STARTING_HP =  100;
-static const int  STARTING_MONEY = 500;
+static const int  PLAYER_STARTING_HP =  100;
+static const int  Player_STARTING_MONEY = 500;
 
 static const int PNJ_VALUE = 10;
 static const int PNJ_DAMAGE= 10;
