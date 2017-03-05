faaada90b881ad166c8e5615d3d7d986000d2ac7
Jepsiko <Jepsiko@Gmail.com>
2017-03-05 11:24:03 +0100

    update

--- Utiles/exe.sh
+++ Utiles/exe.sh
@@ -15,8 +15,11 @@ How to use the script :
 If you want to put all the clients in new tabs instead of dividing the screen by 4 :
     ./exe.sh SHOW
 
-If you want to register the accounts "1", "2", "3", "4" :
+If you want to register the accounts "1", "2", "3", "4" (only used for the first time) :
     ./exe.sh REGISTER
+
+If you want to login the accounts "1", "2", "3", "4" :
+    ./exe.sh LOGIN
 
 If you want to launch a classic game :
     ./exe.sh PLAY
@@ -31,21 +34,29 @@ If you want to launch a team game run this command :
 function forAllClients {
     if [ "${show}" = "SHOW" ]
     then
-        for i in `seq 1 4`;
+        for i in $(seq 1 4);
         do
-            xdotool type --delay 100 "$1"
+            xdotool type --delay ${delayTyping} "$1"
             xdotool key "Return"
-            sleep 1
             xdotool key ctrl+Page_Down
         done
         xdotool key ctrl+Page_Down # Jump over the server tab
     else
         xdotool key alt+g  # TODO: Cette ligne ne marche pas sur Ubuntu. Ca active l'UI de ubuntu
-        xdotool type --delay 100 "$1"
+        xdotool type --delay ${delayTyping} "$1"
         xdotool key "Return"
         xdotool key alt+o
     fi
 }
+
+
+# Global variables
+show=""
+delayTyping=100 # In millisecond between every keystroke
+
+#executableDirectory="~/MEGAsync/GitProjects/Group4/cmake-build-debug/code/"
+executableDirectory="~/CLionProjects/Group4/cmake-build-debug/code/"
+
 
 # Set the terminal on FullScreen
 if [ $(uname -r | grep -c fc) -ne 0 ] # Fedora
@@ -56,7 +67,6 @@ else
 fi
 sleep 1
 
-show=""
 if [ "$1" = "SHOW" ]
 then
     # Open 3 tabs
@@ -84,21 +94,20 @@ else
     xdotool key super+g
 fi
 
-#executableDirectory="~/MEGAsync/GitProjects/Group4/cmake-build-debug/code/"
-executableDirectory="~/CLionProjects/Group4/cmake-build-debug/code/"
 
 # Create a new tab and run the server
 xdotool key ctrl+shift+t
 sleep 1
-xdotool type --delay 100 "cd $executableDirectory"
+xdotool type --delay ${delayTyping} "cd $executableDirectory"
 xdotool key "Return"
-xdotool type --delay 100 "./server"
+xdotool type --delay ${delayTyping} "./server"
 xdotool key "Return"
 sleep 1
 xdotool key ctrl+Page_Down
 
+
 # Run all the clients
-forAllClients "cd $executableDirectory"
+forAllClients "cd ${executableDirectory}"
 forAllClients "./client 127.0.0.1"
 
 
@@ -108,9 +117,9 @@ then
     forAllClients "2"
 
     # Register all the players
-    for i in `seq 1 4`;
+    for i in $(seq 1 4);
     do
-        for j in `seq 1 2`;
+        for j in $(seq 1 2);
         do
             xdotool type ${i}
             xdotool key "Return"
@@ -131,7 +140,7 @@ then
 
     shift
 else
-    if [ "$1" = "LOGIN" ]
+    if [ "$1" = "LOGIN" ] || [ "$1" = "PLAY" ]
     then
         forAllClients "1"
     fi
@@ -140,9 +149,9 @@ fi
 if [ "$1" = "LOGIN" ] || [ "$1" = "PLAY" ]
 then
     # Connect all the players
-    for i in `seq 1 4`;
+    for i in $(seq 1 4);
     do
-        for j in `seq 1 2`;
+        for j in $(seq 1 2);
         do
             xdotool type ${i}
             xdotool key "Return"
@@ -165,6 +174,7 @@ then
     then
         shift
     fi
+
 
     # Launch a game
     if [ "$1" = "PLAY" ]
