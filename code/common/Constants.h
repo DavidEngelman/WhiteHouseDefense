ce98c0bed66125c6cd62c5c66b47f30bf0500c0e
Jepsiko <Jepsiko@Gmail.com>
2017-03-05 19:55:19 +0100

    Le jeu est plus dur

--- code/common/Constants.h
+++ code/common/Constants.h
@@ -13,37 +13,45 @@ static const std::string QUADRANT_NAMES[4] = {"NORTH", "EAST", "SOUTH", "WEST"};
 const static int PARTNERS[4] = {EAST, NORTH, WEST, SOUTH};
 
 
-static const int PNJ_STARTING_HEALTHPOINTS= 100;
+static const int PNJ_STARTING_HEALTHPOINTS = 100;
 static const int PNJ_STARTING_MOVESPEED = 100;
 
-static const int  PLAYER_STARTING_HP =  100;
-static const int  PLAYER_STARTING_MONEY = 200;
+static const int PLAYER_STARTING_HP =  100;
+static const int PLAYER_STARTING_MONEY = 200;
 
 static const int PNJ_VALUE = 10;
-static const int PNJ_DAMAGE= 10;
+static const int PNJ_DAMAGE = 5;
 
 static const char PLACING_TOWER[2] = "t";
 static const char WAVE[2] = "w";
 
 static const int GOLD_EARNED_BY_TICK= 1;
 
-static const float PERCENTAGE_RECOVERED_MONEY= (const float) (50 / 100.0);
+static const float PERCENTAGE_RECOVERED_MONEY = (const float) (50 / 100.0);
 
 // Constant of the towers
-const int GUN_TOWER_PRICE = 50;
-const int GUN_TOWER_RANGE = 4;
-const int GUN_TOWER_DAMAGE = 20;
+static const int GUN_TOWER_PRICE = 50;
+static const int GUN_TOWER_RANGE = 4;
+static const int GUN_TOWER_DAMAGE = 10;
 
-const int SNIPER_TOWER_PRICE = 100;
-const int SNIPER_TOWER_RANGE = 10;
-const int SNIPER_TOWER_DAMAGE = 10;
+static const int SNIPER_TOWER_PRICE = 100;
+static const int SNIPER_TOWER_RANGE = 10;
+static const int SNIPER_TOWER_DAMAGE = 5;
 
-const int SHOCK_TOWER_PRICE = 150;
-const int SHOCK_TOWER_RANGE = 3;
-const int SHOCK_TOWER_DAMAGE = 5;
+static const int SHOCK_TOWER_PRICE = 150;
+static const int SHOCK_TOWER_RANGE = 3;
+static const int SHOCK_TOWER_DAMAGE = 5;
+
+// Future tower
+
+static const int MISSILE_TOWER_PRICE = 150;
+static const int MISSILE_TOWER_RANGE = 6;
+static const int MISSILE_TOWER_DAMAGE = 15;
+
+
 
 static const int STEP_DURATION_IN_MS = 1000;
-static const int INITIAL_NUMBER_OF_PNJS_PER_WAVE = 5;
+static const int INITIAL_NUMBER_OF_PNJS_PER_WAVE = 4;
 static const int TIMED_GAME_DURATION = 120;
 
 static const int NUM_PLAYERS = 4;
