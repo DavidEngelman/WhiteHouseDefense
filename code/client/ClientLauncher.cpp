6eea82bde1e2066ccf612456d578927b70212342
Jepsiko <Jepsiko@Gmail.com>
2017-03-09 12:16:09 +0100

    j'ai un peu modifié le clientLauncher en rajoutant une condition si on entre "-c" ou "--console" en argument

--- code/client/ClientLauncher.cpp
+++ code/client/ClientLauncher.cpp
@@ -3,9 +3,14 @@
 #include "GameManager.hpp"
 
 int main(int argc, char *argv[]) {
-    if (argc <= 1){
+    if (argc == 1){
         std::cerr << "Don't forget the ip_adress of the server as argument! ;)" << std::endl;
         exit(1);
+    } else if (argc == 3) {
+        if (argv[2] == "-c" or argv[2] == "--console") {
+            std::cout << "Console mode" << std::endl;
+            //TODO: qqch pour ne pas lancer les GUI mais lancer les UI à la place
+        }
     }
 
     App app(argv[1]);
