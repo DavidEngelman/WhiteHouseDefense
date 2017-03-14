3d1ae664d3bf9573ccbbd939f72d19b8449449fa
Benjamin <Benjamin.Engelman@ulb.ac.be>
2017-03-14 18:16:01 +0100

    fixed typo

--- code/client/ClientLauncher.cpp
+++ code/client/ClientLauncher.cpp
@@ -34,7 +34,7 @@ int main(int argc, char *argv[]) {
 
     if (!isConsole) {
         QApplication application(argc, argv);
-        application.setApplicationName("Withe house defense");
+        application.setApplicationName("White house defense");
         App app(argv[1]);
         std::cout << argv[1] << std::endl;
         return application.exec();
