ac26868d62abfe5196afb34d7cdf9867cd2e9d27
Jepsiko <Jepsiko@Gmail.com>
2017-03-18 13:14:10 +0100

    le menu des tours ce désactive quand on séléctionne la base

--- code/client/MapGUI.cpp
+++ code/client/MapGUI.cpp
@@ -136,7 +136,7 @@ void MapGUI::mousePressEvent(QMouseEvent* event) {
         gameGUI->disableTowerShop();
     } else {
         highlighted = pos;
-        if (isObstacle(pos) or isPath(pos)) gameGUI->disableTowerShop();
+        if (isObstacle(pos) or isPath(pos) or isBase(pos)) gameGUI->disableTowerShop();
         else gameGUI->enableTowerShop();
     }
 }
