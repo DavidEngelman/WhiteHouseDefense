45f070c645541ae0e32af0f3e32b012ce47f1e0b
harokb <harokb@gmail.com>
2017-03-25 16:25:55 +0100

    Fails gracefully if can't connect to the server.

--- code/client/Other/NetworkedManager.cpp
+++ code/client/Other/NetworkedManager.cpp
@@ -4,6 +4,19 @@
 
 NetworkedManager::NetworkedManager(int port, App* my_app) :AbstractManager(my_app), port(port) {
     server_socket  = init_connection_to_server(my_app->getIp(), port);
+
+    if (server_socket == -1){
+        if (isConsole) {
+            std::cout << "Couldn't connect to the server. Exiting..." << std::endl;
+            exit(0);
+        } else {
+            QMessageBox msgBox;
+            msgBox.setText("Couldn't connect to the server. Exiting...");
+            msgBox.setStandardButtons(QMessageBox::Ok);
+            msgBox.exec();
+            exit(0);
+        }
+    }
 }
 
 
