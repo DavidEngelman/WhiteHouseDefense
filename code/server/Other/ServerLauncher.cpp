1d9d90ef90f0ff2f2156fa81443a3a6c79c074d8
Benjamin <Benjamin.Engelman@ulb.ac.be>
2017-03-30 22:12:41 +0200

    Changed database location for easier compilation

--- code/server/Other/ServerLauncher.cpp
+++ code/server/Other/ServerLauncher.cpp
@@ -4,7 +4,7 @@
 
 
 void * launch_account_server(void * dummy){
-    AccountServer accountServer(5555, "database.db");
+    AccountServer accountServer(5555, "../../database/database.db");
     accountServer.run();
     return nullptr;
 }
